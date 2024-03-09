8c209c2482cd952e2f367a15065598104f51c506
Ali Jardine <jardine64@gmail.com>
2024-03-09 12:38:55 +0000

    Remove the databases folder.

--- cw-db/src/main/java/edu/uob/CommandHandler.java
+++ cw-db/src/main/java/edu/uob/CommandHandler.java
@@ -1,6 +1,12 @@
 package edu.uob;
 
+import java.util.ArrayList;
+
 public class QueryHandler {
+    ArrayList<String> tokens;
+    public QueryHandler(String query) {
+        this.tokens = new Tokenizer(query).getTokens();
+    }
 }
 
 
